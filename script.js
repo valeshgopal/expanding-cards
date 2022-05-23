30d2d663ec8b3645b52eb0b770d4d3faa3c75c05
Valesh Gopal <valesh.dev@gmail.com>
2022-05-23 11:32:38 +0530

    Add expanding cards app

---
+++ script.js
@@ -0,0 +1,14 @@
+const panels = document.querySelectorAll(".panel");
+
+const removeActiveClass = function () {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+};
+
+panels.forEach((panel) =>
+  panel.addEventListener("click", function () {
+    removeActiveClass();
+    panel.classList.add("active");
+  })
+);
